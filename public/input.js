1f98b133a6b699852fba2bd1d746c16cc466bcba
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-25 00:32:47 +0000

    set up handeleUserInput function to hendel only exit, mooving and messases keys

--- public/input.js
+++ public/input.js
@@ -1,22 +1,24 @@
-const { MOVES, isValidKey } = require('./constants')
+const { MOVES, isMovingKey, isMessageKey, CLIENT_MESSAGES } = require('./constants');
 
-const setupInput = function(conn) { 
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  handeleUserInput(conn)
+  handeleUserInput(conn);
   return stdin;
-}
+};
 
-const handeleUserInput = function(conn){
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
+const handeleUserInput = function(conn) {
+  process.stdin.on('data', (k) => {
+    if (k === '\u0003') {
       process.exit();
-    } else if (isValidKey(key)) {
-      conn.write(MOVES[key]);
+    } else if (isMovingKey(k)) {
+      conn.write(MOVES[k]);
+    } else if (isMessageKey(k)) {
+      conn.write(CLIENT_MESSAGES[K]);
     }
-  })
-}
+  });
+};
 
 module.exports = {setupInput};
